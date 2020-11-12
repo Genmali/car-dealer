e6f4aa5768d48a32ee5f1a708621480c11f8ed98
Nicolai <nicolai.dannemann@hotmail.com>
2020-11-12 23:30:50 +0100

    Added Edit and Delete functions to all elements

--- CarDealer/Controllers/SalesPersonsController.cs
+++ CarDealer/Controllers/SalesPersonsController.cs
@@ -67,5 +67,90 @@ namespace CarDealer.Controllers
             }
             return View(salesPerson);
         }
+
+        // GET: SalesPersons/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var salesPerson = await _context.SalesPerson.FindAsync(id);
+            if (salesPerson == null)
+            {
+                return NotFound();
+            }
+            return View(salesPerson);
+        }
+
+        // POST: SalesPersons/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("SalesPersonId,Name,JobTitle,Address,Salary")] SalesPerson salesPerson)
+        {
+            if (id != salesPerson.SalesPersonId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(salesPerson);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!SalesPersonExists(salesPerson.SalesPersonId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(salesPerson);
+        }
+
+        // GET: SalesPersons/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var salesPerson = await _context.SalesPerson
+                .FirstOrDefaultAsync(m => m.SalesPersonId == id);
+            if (salesPerson == null)
+            {
+                return NotFound();
+            }
+
+            return View(salesPerson);
+        }
+
+        // POST: SalesPersons/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var salesPerson = await _context.SalesPerson.FindAsync(id);
+            _context.SalesPerson.Remove(salesPerson);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool SalesPersonExists(int id)
+        {
+            return _context.SalesPerson.Any(e => e.SalesPersonId == id);
+        }
     }
 }
